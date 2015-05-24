6abaf6107210edc96d59d5f9fbc5b73426953eab
z_wlin <280752789@qq.com>
2015-05-24 23:46:08 +0800

    final 1

--- cachematrix.R
+++ cachematrix.R
@@ -3,12 +3,32 @@
 ## 此函数用于创建可缓存逆矩阵的特殊“矩阵”对象
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, 
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## 此函数用于计算makeCacheMatrix返回的特殊“矩阵”的逆矩阵
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data )
+    x$setinverse(m)
+    m
 }
