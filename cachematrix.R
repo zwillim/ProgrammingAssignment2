5770c9b116fc5f8a946ad4704120aaefdb90021b
z_wlin <280752789@qq.com>
2015-05-24 23:10:55 +0800

    work update idx 0

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 缓存逆矩阵
 
-## Write a short comment describing this function
+## 此函数用于创建可缓存逆矩阵的特殊“矩阵”对象
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## 此函数用于计算makeCacheMatrix返回的特殊“矩阵”的逆矩阵
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
